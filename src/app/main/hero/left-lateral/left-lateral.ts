1ce86ebbe696f63f657eba645c121223ab061771
Daniel <Danielluzius@mail.de>
2025-09-25 16:35:15 +0200

    feat: new components for hero section

---
+++ src/app/main/hero/left-lateral/left-lateral.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-lateral',
+  imports: [],
+  templateUrl: './left-lateral.html',
+  styleUrl: './left-lateral.scss'
+})
+export class LeftLateral {
+
+}
