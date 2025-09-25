87d146f3e4aa829418bb7d23702be4b35982bdbc
Daniel <Danielluzius@mail.de>
2025-09-25 13:50:56 +0200

    feat: file stukture

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
+import { Hero } from './main/hero/hero';
+import { AboutMe } from './main/about-me/about-me';
+import { Skills } from './main/skills/skills';
+import { Projects } from './main/projects/projects';
+import { References } from './main/references/references';
+import { Contact } from './main/contact/contact';
+
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Hero, AboutMe, Skills, Projects, References, Contact],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('portfolio');
---
+++ src/app/main/references/references.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-references',
+  imports: [],
+  templateUrl: './references.html',
+  styleUrl: './references.scss',
+})
+export class References {}
