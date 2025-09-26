fde6e19e66e5516f2e36f6bc1b6004e6d14de1f7
Daniel <Danielluzius@mail.de>
2025-09-26 16:21:10 +0200

    feat: added who-i-am and photo component for better structure. added new icon for about me section to project

---
+++ src/app/main/about-me/who-i-am/who-i-am.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-who-i-am',
+  imports: [],
+  templateUrl: './who-i-am.html',
+  styleUrl: './who-i-am.scss'
+})
+export class WhoIAm {
+
+}
